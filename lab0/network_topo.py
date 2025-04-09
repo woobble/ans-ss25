1e6f1352b332b4cbf1b315ea9f21433bf5cf360e
Lin Wang <lin.wang@uni-paderborn.de>
2025-04-09 14:15:23 +0200

    Release lab0

---
+++ lab0/network_topo.py
@@ -0,0 +1,35 @@
+"""
+ Copyright (c) 2025 Computer Networks Group @ UPB
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ """
+
+#!/usr/bin/python
+
+from mininet.topo import Topo
+
+class BridgeTopo(Topo):
+    "Creat a bridge-like customized network topology according to Figure 1 in the lab0 description."
+
+    def __init__(self):
+
+        Topo.__init__(self)
+
+        # TODO: add nodes and links to construct the topology; remember to specify the link properties
+
+topos = {'bridge': (lambda: BridgeTopo())}
